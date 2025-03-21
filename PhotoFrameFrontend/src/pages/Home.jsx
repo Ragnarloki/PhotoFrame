9af01b612140e86fed2ca23129b1460dfc0d8d63
Ragnarloki <sivasankarswaminathan62@gmail.com>
2025-03-21 14:55:17 +0530

    Adding backend code

--- PhotoFrameFrontend/src/pages/Home.jsx
+++ PhotoFrameFrontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/products");
+        const response = await axios.get("https://photoframe-1.onrender.com/api/products");
         setProducts(response.data);
       } catch (error) {
         console.error("Error fetching products:", error);
